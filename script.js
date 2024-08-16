b8af9b1490748e4c1eec3a26e7e61d9dc589dc01
odi <101469962+0xip@users.noreply.github.com.>
2024-08-16 12:48:26 +0300

    created DOM structure

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
- 
+const convertBtn = document.getElementById("convert-btn");
+const input = document.getElementById("number")
+const output = document.getElementById("output");
+
+convertBtn.addEventListener("click", (convertBtn)=>{
+    if(!input.value){
+        output.innerText = "Please enter a valid number";
+    }else if(input.value < 0){
+        output.innerText = "Please enter a number greater than or equal to 1"
+    }else if(input.value > 3999){
+        output.innerText = "Please enter a number less than or equal to 3999"
+    }
+})
